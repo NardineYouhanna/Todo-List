928b68f7cb4dbae7047df0609929fee7160d8f35
NardineYouhanna <64501661+NardineYouhanna@users.noreply.github.com>
2022-01-04 21:10:56 +0200

    Added IP

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 var express = require("express");
 var mongoose = require("mongoose");
+const path = require('path');
 var bodyParser = require("body-parser");
 var app = express();
+
+//app.set('views', path.join(__dirname  , 'public'));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("../frontend/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb://Nardine:nardine123@172.30.172.159:27017/todolistDB");
 
 const itemSchema = {
   name: String,
 };
 
+const users = [ {name: 'u1', password: '123'},  {name: 'u2', password: '123'}]
+
+
 const Item = mongoose.model("Item", itemSchema);
-const item1 = new Item({ name: "Base of list" });
+const item1 = new Item({ name: "Welcome to ItBuddies" });
+const item2 = new Item({ name: "Like, Share and Subscribe" });
+const item3 = new Item({ name: "Enjoy learning" });
 
-
-const d = [item1];
+const d = [item1, item2, item3];
 
 app.get("/", function (req, res) {
   Item.find({}, function (err, f) {
-    if (f.length == 0) {
+    if (f.length === 0) {
       Item.insertMany(d, function (err) {
         if (err) {
           console.log(err);
@@ -35,13 +42,36 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-    i = req.body.n;  //condition is here
-    if(i.length != 0){
+  i = req.body.n;  //condition is here
+  if(i.length != 0){
   const item = new Item({name: i, });
-     item.save();
-  res.redirect("/");
-    }
+
+   item.save();
+    res.redirect("/");
+  }
 });
+
+app.post("/login",function(req,res) {
+  for (let index = 0; index < users.length; index++) {
+    const element = users[index];
+
+
+    if(element.name == req.body.userrname){
+      if(element.password == req.body.password){
+        //redirect
+        console.log("logged in success");
+        res.redirect("tasks");
+      }else{
+        // 
+      }
+    }
+  }
+  
+
+
+});
+
+
 
 app.post("/delete", function (req, res) {
   Item.findByIdAndRemove(req.body.checkbox, function (err) {
@@ -51,69 +81,6 @@ app.post("/delete", function (req, res) {
     }
   });
 });
-/*
-app.put("/edit" , function (req, res){
-    Item.findByIdAndUpdate(req.body.checkbox, function (err) {
-        if (!err) {
-            i = req.body.n;  //condition is here
-            if(i.length != 0)
-            {
-             const item = new Item({name: i, });
-             item.save();
-             console.log("Successfully updated");
-             res.redirect("/");
-           }
-        };
-    })
-});
-
-
-app.post("/login" , function(req , res ){
-
-  if (!email || !password) return res.send("Please enter all the fields");
-
-  const Exits = await User.findOne({ email });
-
-    if (!Exits) return res.send("invalid username or password");
-
-    const doesPasswordMatch = await bcrypt.compare(
-      password,
-      doesUserExits.password
-    );
-
-    if (!doesPasswordMatch) return res.send("invalid useranme or password");
-
-});
-
-app.post("/register" , function(req , res ){
-  const { email, password } = req.body;
-
-    
-    if (!email || !password) return res.send("Please enter all the fields");
-
-    const doesUserExitsAlreay = await User.findOne({ email });
-
-    if (doesUserExitsAlreay) return res.send("A user with that email already exits please try another one!");
-
-
-    const latestUser = new User({ email, password: hashedPassword });
-
-    latestUser.save();
-    latestUser.then(() => {
-        res.send("registered account!");
-        res.redirect("/login");
-      })
-      .catch((err) => console.log(err));
-  });
-
-
-
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  req.session = null;
-  res.redirect('/');
-});
-*/
 
 app.listen(3000, function () {
   console.log("listening on port 3000.");
